1eef11bd81bb4d71eda4602cc26eb78b78e6a6fa
antonior56-only <antonior56@gmail.com>
2026-09-21 17:15:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'casa-in-campagna-v1';
+const CACHE_NAME = 'casa-in-campagna-v2';
 const APP_FILES = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
